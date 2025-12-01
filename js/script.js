c14829728b4736a4be376fc1ec603dad0a06a07a
Sofielindhardt <eaa25soli@students.eaaa.dk>
2025-12-01 16:36:00 +0100

    review sektion

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const getEgenskaberImg = document.getElementById("billedeEgenskaber");
+
+if (getEgenskaberImg) {
+    billedeEgenskaber.addEventListener("click", function () {
+        this.classList.add("scaleUp");
+    });
+}
+        
