e220e027e4ea2c775e5c78d3d2b74570cc77f4c3
Sofielindhardt <eaa25soli@students.eaaa.dk>
2025-12-02 10:10:17 +0100

    skrevet nogle kommentar i js og html

--- js/script.js
+++ js/script.js
@@ -4,29 +4,47 @@
 const zoomImages = document.querySelectorAll(".zoom-img");
 
 // Tilføj event listeners til hvert billede
+
+
+//Mouseover = zoom in
 zoomImages.forEach((img) => {
     img.addEventListener("mouseover", () => {
     img.classList.add("hover-zoom");
     });
+
+
+//Mouseout = remove zoom
 
     img.addEventListener("mouseout", () => {
     img.classList.remove("hover-zoom");
     });
 });
 
+// Hent knappen
+
 const button = document.querySelector(".btn");
+
+
+// Tilføj event listeners til knappen
+
+//Mouseover = zoom in
 
 if (button) {
     button.addEventListener("mouseover", () => {
         button.classList.add("hover-zoom");
     });
 
+
+//Mouseout = remove zoom
+
     button.addEventListener("mouseout", () => {
         button.classList.remove("hover-zoom");
     });
 }
 
-//
+
+
+
  
 
 
